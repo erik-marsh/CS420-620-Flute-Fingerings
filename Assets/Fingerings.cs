059e0ee60336474d23f87faa0107b8901a75cd1e
Erik Marsh <erik.i.marsh@gmail.com>
2022-04-15 20:00:18 -0700

    update fingering class to store note frequency and its midi note value

--- Assets/Fingerings.cs
+++ Assets/Fingerings.cs
@@ -5,8 +5,21 @@ using System.Linq;
 
 public class Fingering
 {
-    //public enum Keys : uint
-    //{
+    public string name;
+    public float frequency;
+    public int midiNote;
+    public uint keyCombination;
+
+    public Fingering(string name, float frequency, int midiNote, uint keyCombination)
+    {
+        this.name = name;
+        this.frequency = frequency;
+        this.midiNote = midiNote;
+        this.keyCombination = keyCombination;
+    }
+
+    #region Static Data
+    // could have been an enum but it just made the fingering assignment way too verbose
     public static uint LIndex = 1 << 0;
     public static uint LMiddle = 1 << 1;
     public static uint LRing = 1 << 2;
@@ -17,34 +30,27 @@ public class Fingering
     public static uint RRing = 1 << 7;
     public static uint RPinky = 1 << 8;
     public static uint RThumb = 1 << 9;
-    //}
+
 
     public static Fingering[] knownFingerings = {
-    /*public static Fingering C5  = */new Fingering("C5", LIndex | RPinky),
-    /*public static Fingering Db5 = */new Fingering("Db5", RPinky),
-    /*public static Fingering D5  = */new Fingering("D5", LThumb | LMiddle | LRing | RIndex | RMiddle | RRing),
-    /*public static Fingering Eb5 = */new Fingering("Eb5", LThumb | LIndex | LMiddle | LRing | RIndex | RMiddle | RRing | RPinky),
-    /*public static Fingering E5  = */new Fingering("E5", LThumb | LIndex | LMiddle | LRing | RIndex | RMiddle | RPinky),
-    /*public static Fingering F5  = */new Fingering("F5", LThumb | LIndex | LMiddle | LRing | RIndex | RPinky),
-    /*public static Fingering Gb5 = */new Fingering("Gb5", LThumb | LIndex | LMiddle | LRing | RRing | RPinky),
-    /*public static Fingering G5  = */new Fingering("G5", LThumb | LIndex | LMiddle | LRing | RPinky),
-    /*public static Fingering Ab5 = */new Fingering("Ab5", LThumb | LIndex | LMiddle | LRing | LPinky | RPinky),
-    /*public static Fingering A5  = */new Fingering("A5", LThumb | LIndex | LMiddle | RPinky),
-    /*public static Fingering Bb5 = */new Fingering("Bb5", LThumb | LIndex | RIndex | RPinky),
-    /*public static Fingering B5  = */new Fingering("B5", LThumb | LIndex | RPinky)
+        new Fingering("C5",  523.25f, 72, LIndex | RPinky),
+        new Fingering("Db5", 554.37f, 73, RPinky),
+        new Fingering("D5",  587.33f, 74, LThumb | LMiddle | LRing | RIndex | RMiddle | RRing),
+        new Fingering("Eb5", 622.25f, 75, LThumb | LIndex | LMiddle | LRing | RIndex | RMiddle | RRing | RPinky),
+        new Fingering("E5",  659.26f, 76, LThumb | LIndex | LMiddle | LRing | RIndex | RMiddle | RPinky),
+        new Fingering("F5",  698.46f, 77, LThumb | LIndex | LMiddle | LRing | RIndex | RPinky),
+        new Fingering("Gb5", 739.99f, 78, LThumb | LIndex | LMiddle | LRing | RRing | RPinky),
+        new Fingering("G5",  783.99f, 79, LThumb | LIndex | LMiddle | LRing | RPinky),
+        new Fingering("Ab5", 830.61f, 80, LThumb | LIndex | LMiddle | LRing | LPinky | RPinky),
+        new Fingering("A5",  880.00f, 81, LThumb | LIndex | LMiddle | RPinky),
+        new Fingering("Bb5", 932.33f, 82, LThumb | LIndex | RIndex | RPinky),
+        new Fingering("B5",  987.77f, 83, LThumb | LIndex | RPinky)
     };
 
-    public static Fingering nullFingering = new Fingering("", 0);
+    public static Fingering nullFingering = new Fingering("", 0.0f, 0, 0);
+    #endregion
 
-    public uint keyCombination;
-    public string name;
-
-    public Fingering(string name, uint keyCombination)
-    {
-        this.name = name;
-        this.keyCombination = keyCombination;
-    }
-
+    #region Static Helpers
     public static Fingering GetFingeringByCombination(uint keyCombination)
     {
         // correction to ignore the right thumb, since it doesn't actually press keys
@@ -74,4 +80,5 @@ public class Fingering
 
         return res;
     }
+    #endregion
 }
